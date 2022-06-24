72021c503377caeac825c66c29d8f2b82e25b8c5
Kefseda <108129586+Kefseda@users.noreply.github.com>
2022-06-24 20:22:13 +0900

    Add files via upload

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -7,26 +7,18 @@ public class CameraController : MonoBehaviour
     public Transform target;
     public Vector3 offset;
     public float smoothSpeed = 0.125f;
-    public Vector2 moveSpeed = new Vector2(1,1);
-    public Vector2 moveStrong = new Vector2(5,5);
-    public Vector2 angle = new Vector2(0,0);
-    public bool back = false;
+    public Vector2 moveSpeed = new Vector2(1, 1);
+    public Vector2 moveStrong = new Vector2(5, 5);
+    public Vector2 angle = new Vector2(0, 0);
+    public bool back = true;
+    public float Period;
+    bool gameIsStart;
+    bool pressed;
 
     void Start()
     {
         angle.x = 0;
         angle.y = 0;
-    }
-
-    Vector3 RYtoXZ(float r)
-    { 
-        float x = 2 * Mathf.PI / 360 * r;
-        return new Vector3(Mathf.Cos(x), 0, Mathf.Sin(x));
-    }
-    Vector3 RZtoXY(float r)
-    {
-        float x = 2 * Mathf.PI / 360 * r;
-        return new Vector3(Mathf.Cos(x), Mathf.Sin(x), 0);
     }
 
     float normalizeRotate(float r)
@@ -36,31 +28,57 @@ public class CameraController : MonoBehaviour
         return r;
     }
 
-    void FixedUpdate()
+
+    void LateUpdate()
     {
-        if (back)
-        {
-            transform.position = Vector3.Lerp(transform.position, 
-                target.position + new Vector3(target.right.x * moveStrong.x, offset.y, target.right.z * moveStrong.x), smoothSpeed);
-        }
-        else
-        {
-            Vector3 v = RYtoXZ(angle.x) + RZtoXY(angle.y);
-            transform.position = Vector3.Lerp(transform.position, 
-                target.position + offset + ((new Vector3(v.x/2*moveStrong.x,v.y*moveStrong.y,moveStrong.x*v.z) * moveStrong.y)), smoothSpeed);
-        }
         transform.LookAt(target);
+
+        if (!back && pressed && gameIsStart)
+        {
+            transform.RotateAround(target.position,
+                                       Vector3.up,
+                                       normalizeRotate(Input.GetAxis("Mouse X") * 5));
+
+            transform.RotateAround(target.position,
+                                            new Vector3(1, 0, 1),
+                                            -Input.GetAxis("Mouse Y") * 5);
+        }
+
 
         if (Input.GetMouseButton(1))
         {
             back = false;
+            pressed = true;
             angle.x -= Input.GetAxis("Mouse X") * moveSpeed.x;
             angle.x = normalizeRotate(angle.x);
             angle.y -= Input.GetAxis("Mouse Y") * moveSpeed.y;
             angle.y = normalizeRotate(angle.y);
         }
+        else pressed = false;
+
+    }
 
 
 
+    private void FixedUpdate()
+    {
+        
+
+        if (!gameIsStart)
+        {
+            float CornerSpeed = (Mathf.PI * 2 / Period) * Time.time;
+            transform.Translate(new Vector3(Mathf.Cos(CornerSpeed) * 10, 0, Mathf.Sin(CornerSpeed) * 10) * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, target.position) < 5) gameIsStart = true;
+        }
+        else
+        {
+
+            if (back)
+            {
+                transform.position = Vector3.Lerp(transform.position,
+                    target.position + new Vector3(target.right.x * moveStrong.x, offset.y, target.right.z * moveStrong.x), smoothSpeed);
+            }
+        }
     }
 }
